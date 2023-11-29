633828cf30c6baff61ab1d0787a02ed989695ad4
gabrik <gabriele.baldoni@gmail.com>
2023-11-29 18:12:24 +0100

    initial

---
+++ clients/python/src/relase-learning-py/__init__.py
@@ -0,0 +1,5 @@
+def hello():
+    print("hello world\n")
+
+def hello_new():
+    print("Hello world,2 \n")
---
+++ src/main.rs
@@ -0,0 +1,8 @@
+fn main() {
+    println!("Hello, world! 2");
+    println!("1+1={}", 1 + 1);
+    println!("2+2={}", 2 + 2);
+    println!("3+3={}", 3 + 3);
+    println!("4+4={}", 4 + 4);
+    println!("5+5={}", 5 + 5);
+}
